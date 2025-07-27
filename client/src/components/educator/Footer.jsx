b2a186942a5f282958ddee1819a57a4a8f24d8d5
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-07-27 21:43:31 +0530

    footer update

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
     <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t border-gray-400'>
       {/* left */}
       <div className='flex items-center gap-4'>
-        <img className='hidden md:block w-20'
-          src={assets.logo} alt="" />
+        <img src={assets.logo_frame} alt="" className='w-32 ' />
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
           Copyright 2025 © Agnik Mukherjee. | All Rights Reserved.
